ab6f9ce76d45b875e744cf4e1968eb0c21e58f6c
Dominik Chrastecky <dominik.chrastecky@broadcom.com>
2019-12-05 11:16:37 +0100

    add some tests

--- tests/Iban/IbanBicPairTest.php
+++ tests/Iban/IbanBicPairTest.php
@@ -4,13 +4,18 @@ namespace rikudou\SkQrPayment\Tests\Iban;
 
 use Rikudou\Iban\Iban\CzechIbanAdapter;
 use Rikudou\Iban\Iban\IBAN;
+use rikudou\SkQrPayment\Exception\BicNotFoundException;
+use rikudou\SkQrPayment\Exception\DictionaryNotFoundException;
+use rikudou\SkQrPayment\Exception\QrPaymentException;
 use rikudou\SkQrPayment\Iban\IbanBicPair;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class IbanBicPairTest extends TestCase
 {
 
     private const VALID_STRING_IBAN = 'CZ7061000000001030900063';
+    private const VALID_SK_IBAN = 'SK6807200002891987426353';
 
     public function testValidStringIbanNoBic()
     {
@@ -18,6 +23,10 @@ class IbanBicPairTest extends TestCase
 
         self::assertEquals(self::VALID_STRING_IBAN, $instance->asString());
         self::assertEquals('EQBKCZPP', $instance->getBic());
+
+        $instance = new IbanBicPair(self::VALID_SK_IBAN);
+        self::assertEquals(self::VALID_SK_IBAN, $instance->asString());
+        self::assertEquals('NBSBSKBX', $instance->getBic());
     }
 
     public function testValidStringIbanWithBic()
@@ -51,9 +60,39 @@ class IbanBicPairTest extends TestCase
         self::assertEquals('TEST', $instance->getBic());
     }
 
-    public function testValidIbanWithUnknownBic()
+    public function testValidIbanWithUnknownCountry()
     {
-
+        $this->expectException(DictionaryNotFoundException::class);
+        new IbanBicPair('NL55ABNA1756700125');
     }
 
+    public function testValidIbanWithUnknownBank()
+    {
+        $this->expectException(BicNotFoundException::class);
+        new IbanBicPair('CZ0750514167419537656264');
+    }
+
+    public function testInvalidIban()
+    {
+        $this->expectException(QrPaymentException::class);
+        new IbanBicPair('CZ7061000000001030900064');
+    }
+
+    public function testInvalidObject()
+    {
+        $this->expectException(QrPaymentException::class);
+        new IbanBicPair(new stdClass());
+    }
+
+    public function testGetValidator()
+    {
+        $iban = new IBAN(self::VALID_STRING_IBAN);
+        $instance = new IbanBicPair($iban);
+
+        if ($iban->getValidator() === null) {
+            self::assertEquals($iban->getValidator(), $instance->getValidator());
+        } else {
+            self::assertEquals(get_class($iban->getValidator()), get_class($instance->getValidator()));
+        }
+    }
 }
---
+++ tests/IbanToBic/Dictionary/AbstractBicDictionaryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace rikudou\SkQrPayment\Tests\Iban\IbanToBic\Dictionary;
+
+use Rikudou\Iban\Iban\IBAN;
+use rikudou\SkQrPayment\IbanToBic\Dictionary\AbstractBicDictionary;
+use PHPUnit\Framework\TestCase;
+
+class AbstractBicDictionaryTest extends TestCase
+{
+    private const VALID_IBAN = 'CZ7061000000001030900063';
+
+    /**
+     * @var AbstractBicDictionary
+     */
+    private $instance;
+
+    protected function setUp()
+    {
+        $this->instance = new class extends AbstractBicDictionary {
+
+            /**
+             * @inheritDoc
+             */
+            protected function getMap(): array
+            {
+                return [
+                    '6100' => 'EQBKCZPP',
+                ];
+            }
+
+            /**
+             * @inheritDoc
+             */
+            public function getCountryCode(): string
+            {
+                return 'CZ';
+            }
+        };
+    }
+
+    public function testGetBic()
+    {
+        self::assertEquals('EQBKCZPP', $this->instance->getBic(new IBAN(self::VALID_IBAN)));
+        // test cache returns the same
+        self::assertEquals('EQBKCZPP', $this->instance->getBic(new IBAN(self::VALID_IBAN)));
+    }
+}
